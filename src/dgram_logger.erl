aba6d224509146981ae4a7c8f05a2e8ef23bb3a1
Peer Stritzinger <peer@stritzinger.com>
2019-07-12 18:34:33 +0200

    Implement report formating including legacy error_logger reports

--- src/dgram_logger.erl
+++ src/dgram_logger.erl
@@ -32,16 +32,19 @@ log(Log_event, #{config := C}) ->
 
 send(Sock, Host, Port, #{level := Level, msg := Msg, meta := Meta}) ->
     Tags = meta_tags(Meta),
-    erlang:display(iolist_to_binary(Tags)),
     Data = case Msg of
                {string, String} ->
                    iolist_to_binary(
                      io_lib:format("sol,level=~p~s msg=\"~s\"\n", 
                                    [Level, Tags, String]));
+               {report, #{report := Report}} ->
+                   iolist_to_binary(
+                     io_lib:format("sol,level=~p~s ~s\n", 
+                                   [Level, Tags, report_values(Report)]));
                {report, Report} ->
                    iolist_to_binary(
-                     io_lib:format("sol,level=~p~s msg=\"~s\"\n", 
-                                   [Level, Tags, "report"]));
+                     io_lib:format("sol,level=~p~s ~s\n", 
+                                   [Level, Tags, report_values(Report)]));
                {Format, Args} when is_list(Args) ->
                    Io_list = io_lib:format(Format, Args),
                    iolist_to_binary(
@@ -53,6 +56,11 @@ send(Sock, Host, Port, #{level := Level, msg := Msg, meta := Meta}) ->
 meta_tags(Meta) ->
     prepend_comma(lists:join($,, maps:fold(fun format_tag/3, [], Meta))).
 
+report_values(Report) when is_list(Report) ->
+    lists:join($,, lists:foldl(fun format_entry/2, [], Report));
+report_values(Report) when is_map(Report) ->
+    lists:join($,, maps:fold(fun format_entry/3, [], Report)).
+    
 prepend_comma([]) ->
     [];
 prepend_comma(List) ->
@@ -64,7 +72,15 @@ format_tag(time, _, Acc) ->
     Acc;
 format_tag(report_cb, _, Acc) ->
     Acc;
+format_tag(error_logger, _, Acc) ->
+    Acc;
 format_tag(Key, Value, Acc) ->
+    [io_lib:format("~p=~p", [Key, Value]) | Acc].
+
+format_entry({Key, Value}, Acc) ->
+    format_entry(Key, Value, Acc).
+
+format_entry(Key, Value, Acc) ->
     [io_lib:format("~p=~p", [Key, Value]) | Acc].
 
 %% Meta_server callbacks
